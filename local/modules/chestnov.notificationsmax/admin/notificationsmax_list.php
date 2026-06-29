5eb4770f288275227d6003771bfa165db13424ed
grawe <grawe.sd42s@gmail.com>
2026-06-29 05:34:55 +0500

    Добавил комментарии.

--- local/modules/chestnov.notificationsmax/admin/notificationsmax_list.php
+++ local/modules/chestnov.notificationsmax/admin/notificationsmax_list.php
@@ -2,23 +2,21 @@
 // 1. Подключаем служебную часть пролога (ядро, сессия, авторизация)
 require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_before.php");
 
-
-
 Bitrix\Main\Localization\Loc::loadMessages(__FILE__);
 
 $mid = "chestnov.notificationsmax";
-
-
-// Проверка подключение модулей и пользователя
 
 $APPLICATION->SetTitle(Bitrix\Main\Localization\Loc::getMessage("CHESTNOV_NOTIFICATIONSMAX_PAGE_TITLE"));
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_admin_after.php");
 
+// Проверка прав пользователя и доступов к админке
 include(__DIR__ . "/include/checks.php");
 
+// Обработка POST действий: save / send test message
 include(__DIR__ . "/include/actions.php");
 
+// Отрисовка формы настроек модуля
 include(__DIR__ . "/include/form.php");
 
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/epilog_admin.php");
